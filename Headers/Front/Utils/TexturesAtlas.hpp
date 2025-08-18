219670d184d4d58e958a129308514b62d43d99f2
mjuin <mjuin@student.42angouleme.fr>
2025-08-18 16:10:32 +0200

    some advencement on texture atlas but need amelioration

--- Headers/Front/Utils/TexturesAtlas.hpp
+++ Headers/Front/Utils/TexturesAtlas.hpp
@@ -24,14 +24,20 @@ namespace Vox::Front::Utils
 			};
 
 			std::vector<TextureInfo> _textureInfos;
-            size_t _textureSize;
+			size_t _textureSize;
+			size_t _textureChannels;
+
+			VkImage _vkImage;
+			VkDeviceMemory _vkMemory;
 
 		public:
-			TexturesAtlas(std::vector<std::string> atlasTextures, size_t textureSize = 32);
-			TexturesAtlas(std::vector<std::string> atlasTextures, uint32_t expectedWidth, size_t textureSize = 32);
+			TexturesAtlas(std::vector<std::string> atlasTextures, size_t textureSize = 32, size_t textureChannels = 4);
+			TexturesAtlas(std::vector<std::string> atlasTextures, uint32_t expectedWidth, size_t textureSize = 32,
+						  size_t textureChannels = 4);
 			const TextureInfo &GetTextureInfo(size_t index) const;
 
 		private:
+            void BuildVkImage(unsigned char *imgData);
 			static uint32_t nextPowerOfTwo(uint32_t value);
 			void BuildAtlas(std::vector<std::string> &&textures);
 	};
--- Sources/Front/Utils/TexturesAtlas.cpp
+++ Sources/Front/Utils/TexturesAtlas.cpp
@@ -1,5 +1,8 @@
 #include "Front/Utils/TexturesAtlas.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "Ext/stb/stb_image.h"
+
 #include <cmath>
 
 namespace Vox::Front::Utils
@@ -9,8 +12,8 @@ namespace Vox::Front::Utils
 		return 1U << (32 - __builtin_clz(value - 1));
 	}
 
-	TexturesAtlas::TexturesAtlas(std::vector<std::string> atlasTextures, size_t textureSize)
-		: _atlasWidth(0), _atlasHeight(0), _textureSize(textureSize)
+	TexturesAtlas::TexturesAtlas(std::vector<std::string> atlasTextures, size_t textureSize, size_t textureChannels)
+		: _atlasWidth(0), _atlasHeight(0), _textureSize(textureSize), _textureChannels(textureChannels)
 	{
 		size_t size = atlasTextures.size();
 		if (size == 0)
@@ -36,8 +39,9 @@ namespace Vox::Front::Utils
 		BuildAtlas(std::move(atlasTextures));
 	}
 
-	TexturesAtlas::TexturesAtlas(std::vector<std::string> atlasTextures, uint32_t expectedWidth, size_t textureSize)
-		: _atlasWidth(0), _atlasHeight(0), _textureSize(textureSize)
+	TexturesAtlas::TexturesAtlas(std::vector<std::string> atlasTextures, uint32_t expectedWidth, size_t textureSize,
+								 size_t textureChannels)
+		: _atlasWidth(0), _atlasHeight(0), _textureSize(textureSize), _textureChannels(textureChannels)
 	{
 		size_t size = atlasTextures.size();
 		if (size == 0)
@@ -72,32 +76,103 @@ namespace Vox::Front::Utils
 		return _textureInfos[index];
 	}
 
+	void TexturesAtlas::BuildVkImage(unsigned char *imgData)
+	{
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+
+		VkDeviceSize imgSize = this->_.x * this->_size.y * this->_imgChannels;
+		Buffer::CreateBuffer(imgSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+							 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer,
+							 stagingBufferMemory);
+
+		VkImageCreateInfo imageInfo{};
+		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageInfo.extent.width = this->_atlasWidth * this->_textureSize;
+		imageInfo.extent.height = this->_atlasHeight * this->_textureSize;
+		imageInfo.extent.depth = 1;
+		imageInfo.mipLevels = 1;
+		imageInfo.arrayLayers = 1;
+		imageInfo.format = this->_textureChannels == 4 ? VK_FORMAT_R8G8B8A8_UNORM : VK_FORMAT_R8_UNORM;
+		imageInfo.tiling = tiling;
+		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageInfo.usage = usage;
+		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create image!");
+		}
+	}
+
 	void TexturesAtlas::BuildAtlas(std::vector<std::string> &&textures)
 	{
 		this->_textureInfos.reserve(textures.size());
-
 		float cellWidth = 1.0f / _atlasWidth;
 		float cellHeight = 1.0f / _atlasHeight;
 
-		for (size_t i = 0; i < textures.size(); ++i)
-		{
-			TextureInfo info;
-			info.uOffset = (i % _atlasWidth) * cellWidth;
-			info.vOffset = (i / _atlasWidth) * cellHeight;
-			info.uSize = cellWidth;
-			info.vSize = cellHeight;
-			this->_textureInfos.push_back(info);
-		}
+		// Allocation de la mémoire pour l'atlas
+		unsigned char atlasData[(this->_textureSize * this->_textureSize * this->_textureChannels) * textures.size()];
 
-        
+		// Remplissage initial avec des pixels blancs
+		std::fill(atlasData,
+				  atlasData + (this->_textureSize * this->_textureSize * this->_textureChannels * textures.size()),
+				  255);
 
 		for (size_t y = 0; y < this->_atlasHeight; y++)
 		{
 			for (size_t x = 0; x < this->_atlasWidth; x++)
 			{
 				size_t index = y * this->_atlasHeight + x;
-                size_t atlasPosY = y * this->_textureSize;
-                size_t atlasPosX = x * this->_textureSize;
+				if (index >= textures.size())
+					break;
+
+				size_t atlasPosY = y * this->_textureSize;
+				size_t atlasPosX = x * this->_textureSize;
+
+				int imgWidth, imgHeight, imgChannels;
+				const unsigned char *imgData =
+					stbi_load(textures[index].c_str(), &imgWidth, &imgHeight, &imgChannels, 0);
+
+				TextureInfo info;
+				info.uOffset = x * cellWidth;
+				info.vOffset = y * cellHeight;
+				info.uSize = cellWidth;
+				info.vSize = cellHeight;
+				this->_textureInfos.push_back(info);
+
+				if (imgData == NULL)
+				{
+					std::cout << "[WARNING] Failed to load image \"" << textures[index]
+							  << "\". Using white placeholder." << std::endl;
+					continue;
+				}
+
+				if (imgWidth != this->_textureSize || imgHeight != this->_textureSize ||
+					imgChannels != this->_textureChannels)
+				{
+					std::cout << "[WARNING] Invalid image format \"" << textures[index]
+							  << "\". Neither img size or channel number is invalid." << std::endl;
+					stbi_image_free((void *)imgData);
+					continue;
+				}
+
+				// Copie des pixels de l'image dans l'atlas
+				for (size_t yTex = 0; yTex < this->_textureSize; yTex++)
+				{
+					for (size_t xTex = 0; xTex < this->_textureSize; xTex++)
+					{
+						size_t srcPos = (yTex * imgWidth + xTex) * this->_textureChannels;
+						size_t dstPos = ((atlasPosY + yTex) * this->_textureSize * this->_textureChannels +
+										 (atlasPosX + xTex) * this->_textureChannels);
+
+						std::copy(imgData + srcPos, imgData + srcPos + this->_textureChannels, atlasData + dstPos);
+					}
+				}
+
+				stbi_image_free((void *)imgData);
 			}
 		}
 	}
