fcf616536bfe7a2c82d829b540eca7325730c950
m-juin <mickael.juin.16@gmail.com>
2025-08-31 20:19:29 +0200

    Adding label to MenuCreate IF

--- Sources/Game/Scenes/Menu/I_MenuCreate.cpp
+++ Sources/Game/Scenes/Menu/I_MenuCreate.cpp
@@ -4,8 +4,8 @@
 
 #include "Front/Interfaces/Elements/Buttons/TexturedButton.hpp"
 #include "Front/Interfaces/Elements/Image.hpp"
-#include "Front/Interfaces/Elements/Text.hpp"
 #include "Front/Interfaces/Elements/InputField.hpp"
+#include "Front/Interfaces/Elements/Text.hpp"
 
 namespace Vox::Game::Scenes::Menu::Interfaces
 {
@@ -24,16 +24,48 @@ namespace Vox::Game::Scenes::Menu::Interfaces
 												 Vox::Front::Interfaces::Elements::Vector2(16, 16)),
 						 0);
 
-        { // IF_Name, IF_Seed
-            InputField::Constructor st{};
-            st.BGAtlas = "Menu_Main";
-            st.BGAtlasKey = "Dirt";
-            st.inputMode = InputField::E_InputMode::AlphaNumeric;
-            st.pos = {this->_pos[0] + this->_size[0] / 2 - 200, this->_pos[1] + 100};
-            st.size = {400, 50};
+		{
+			Text::Vox_Text_Constructor st{};
+			st.content = "Create New World";
+			st.scale = 1.5f;
+			st.pos = {this->_pos[0] + this->_size[0] / 2 - Text::GetTextSize(st.content, st.scale)[0] / 2,
+					   this->_pos[1] + 100 + 25};
+			st.size = {Text::GetTextSize(st.content, st.scale)[0], 50};
 
-            this->AddElement("IF_Name", std::make_unique<InputField>(st), 1);
-        }
+			this->AddElement("TXT_PageName", std::make_unique<Text>(st), 1);
+		}
+
+		{ // IF_Name, IF_Seed
+			InputField::Constructor st{};
+			st.BGAtlas = "Menu_Main";
+			st.BGAtlasKey = "Dirt";
+			st.inputMode = InputField::E_InputMode::AlphaNumeric;
+			st.pos = {this->_pos[0] + this->_size[0] / 2 + 50, this->_pos[1] + 250};
+			st.size = {400, 50};
+
+			this->AddElement("IF_Name", std::make_unique<InputField>(st), 1);
+
+			Text::Vox_Text_Constructor st2{};
+			st2.content = "World Name:";
+			st2.scale = 0.7f;
+			st2.pos = {this->_pos[0] + this->_size[0] / 2 - 50 - Text::GetTextSize(st2.content, st2.scale)[0],
+					   this->_pos[1] + 250 + 25};
+			st2.size = {Text::GetTextSize(st2.content, st2.scale)[0], 50};
+
+			this->AddElement("TXT_Name", std::make_unique<Text>(st2), 1);
+
+			st.inputMode = InputField::E_InputMode::Numeric;
+			st.pos[1] += 100;
+
+			this->AddElement("IF_Seed", std::make_unique<InputField>(st), 1);
+
+			st2.content = "Seed:";
+			st2.pos = {this->_pos[0] + this->_size[0] / 2 - 50 - Text::GetTextSize(st2.content, st2.scale)[0],
+					   st.pos[1] + 25};
+			st2.size = {Text::GetTextSize(st2.content, st2.scale)[0], 50};
+
+			this->AddElement("TXT_Seed", std::make_unique<Text>(st2), 1);
+		}
 	}
 
 	void I_MenuCreate::SetPos(const Vox::Front::Interfaces::Elements::Vector2 newPos)
@@ -41,7 +73,7 @@ namespace Vox::Game::Scenes::Menu::Interfaces
 		if (newPos == this->_pos)
 			return;
 		this->_pos = newPos;
-        this->GetElement<Image>("IMG_BG")->SetPos(this->_pos);
+		this->GetElement<Image>("IMG_BG")->SetPos(this->_pos);
 	}
 
 	void I_MenuCreate::SetSize(const Vox::Front::Interfaces::Elements::Vector2 newSize)
@@ -50,7 +82,7 @@ namespace Vox::Game::Scenes::Menu::Interfaces
 			return;
 		_size[0] = MGL::Utils::findNextMultiple(newSize[0], 16.f);
 		_size[1] = MGL::Utils::findNextMultiple(newSize[1], 16.f);
-        this->GetElement<Image>("IMG_BG")->SetSize(this->_size);
+		this->GetElement<Image>("IMG_BG")->SetSize(this->_size);
 	}
 
 	void I_MenuCreate::Render()
