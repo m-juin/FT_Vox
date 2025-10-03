f0f7980bfb255dbd9f39779371fb23819cf4533c
mjuin <mjuin@student.42angouleme.fr>
2025-10-03 09:39:15 +0200

    Some change on perlin value but probably need full rework of how generation work (perlin octave + per biome erosion)

--- Headers/Game/Scenes/World/Generation/PerlinUtils.hpp
+++ Headers/Game/Scenes/World/Generation/PerlinUtils.hpp
@@ -21,12 +21,12 @@ namespace Vox::Game::Generation::Perlins::Utils
 			{
 			}
 	};
-	constexpr PerlinData ContinentalnessData(1.2f, 0.001f, 8, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 6);
-	constexpr PerlinData ErosionData(1.5f, 0.0008f, 4, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 6);
-	constexpr PerlinData PeaksAndValleyData(0.9f, 0.0008f, 4, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 4);
-	constexpr PerlinData HumidityData(1.0f, 0.0016f, 8, MGL::Vectors::Vector2<float>(1250.0f, -1250.0f), 4);
-	constexpr PerlinData TemperatureData(1.0f, 0.0008f, 8, MGL::Vectors::Vector2<float>(-1250.0f, 1250.0f), 4);
-	constexpr PerlinData WeirdnessData(0.9f, 0.0008f, 4, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 4);
+	constexpr PerlinData ContinentalnessData(1.3f, 0.0005f, 8, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 6);
+	constexpr PerlinData ErosionData(1.0f, 0.0002f, 8, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 6);
+	constexpr PerlinData PeaksAndValleyData(1.5f, 0.0006f, 8, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 4);
+	constexpr PerlinData HumidityData(1.0f, 0.0002f, 4, MGL::Vectors::Vector2<float>(1250.0f, -1250.0f), 4);
+	constexpr PerlinData TemperatureData(1.3f, 0.00008f, 8, MGL::Vectors::Vector2<float>(-1250.0f, 1250.0f), 4);
+	constexpr PerlinData WeirdnessData(1.5f, 0.0006f, 8, MGL::Vectors::Vector2<float>(0.0f, 0.0f), 4);
 
 } // namespace Vox::Game::Generation::Perlins::Utils
 
